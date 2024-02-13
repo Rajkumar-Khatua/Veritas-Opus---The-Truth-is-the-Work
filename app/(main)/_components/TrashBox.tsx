f529bf7d9abb791410368a31d2933dff8e6a57ac
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2024-02-13 12:17:54 +0530

    Implemented to Trash functionality

--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ConfirmModal } from "@/components/models/confrim-model";
 import { Spinner } from "@/components/spinner";
 import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
@@ -97,12 +98,14 @@ export const TrashBox = () => {
               >
                 <Undo2 className="h-4 w-4 text-muted-foreground" />
               </div>
-              <div
-                role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200 "
-              >
-                <Trash className="h-4 w-4 text-muted-foreground" />
-              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200 "
+                >
+                  <Trash className="h-4 w-4 text-muted-foreground" />
+                </div>
+              </ConfirmModal>
             </div>
           </div>
         ))}
