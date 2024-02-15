3008d463743dc764981bbd2bc7de72541bfeda19
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2024-02-15 14:46:13 +0530

    added old dependency

--- components/editor.tsx
+++ components/editor.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes";
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import "@blocknote/react/style.css";
+import "@blocknote/core/style.css";
 
 import { useEdgeStore } from "@/lib/edgestore";
 
