55aacdf4b06013cda85aaa52f1e1ab42988283c3
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2024-02-15 14:21:27 +0530

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,13 +17,13 @@ export const metadata: Metadata = {
     icon: [
       {
         media: "(prefers-color-scheme: light)",
-        url: "/logo.svg",
-        href: "/logo.svg",
+        url: "/logo.png",
+        href: "/logo.png",
       },
       {
         media: "(prefers-color-scheme: dark)",
-        url: "/logo-dark.svg",
-        href: "/logo-dark.svg",
+        url: "/logo.png",
+        href: "/logo.png",
       },
     ],
   },
